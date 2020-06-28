a474a7e28c5957a44a65be28acd7ca16d8aab44b
Esteban Gonzalez <estebangonzalez@MacBook-Pro-de-Esteban.local>
2020-06-28 01:49:58 -0400

    se actualiza desafio 1- parte 2

--- desafio1/desafio1.sql
+++ desafio1/desafio1.sql
@@ -44,16 +44,25 @@ INSERT INTO post(nombreUsuario, fechaCreacion,contenido,descripcion,titulo) VALU
 
 
 -- Parte 2
-
+-- Para la parte 2 se normaliza la tabla post quedado la tabla usurio y post
 
 -- tabla usuario
 CREATE TABLE usuario(id SERIAL, nombre VARCHAR(25), PRIMARY KEY(id));
 
+-- inserta los usuarios
+INSERT INTO usuario(nombre) VALUES ('Pamela');
+INSERT INTO usuario(nombre) VALUES ('Carlos');
+INSERT INTO usuario(nombre) VALUES ('Pedro');
+
+
+
+-- Se elimina la tabla post
+DROP TABLE post;
 
 -- tabla post
 CREATE TABLE post(
 id SERIAL, 
-fecha_creacion DATE, 
+fechacreacion DATE, 
 contenido VARCHAR(25), 
 descripcion VARCHAR(25), 
 titulo VARCHAR(25), 
@@ -62,9 +71,16 @@ PRIMARY KEY (id),
 FOREIGN KEY (idusuario) REFERENCES usuario(id)
 );
 
+-- se insertan datos tabla post para trabajar en el nuevo modelo
+INSERT INTO post(fechaCreacion,contenido,descripcion,titulo,idusuario) VALUES ('2020-06-25', 'Catalogo libros','Libros poemas','50 poemas',1);
+INSERT INTO post(fechaCreacion,contenido,descripcion,titulo,idusuario) VALUES ('2020-06-27', 'Catalogo audios','musica clasica','5th sinfonia',1);
+INSERT INTO post(fechaCreacion,contenido,descripcion,titulo,idusuario) VALUES ('2020-06-28', 'Catalogo revistas','populares','Condorito',2);
+DELETE FROM post WHERE id=3; 
+INSERT INTO post(fechaCreacion,contenido,descripcion,titulo,idusuario) VALUES ('2020-06-28', 'Catalogo vinos','Cabernet','vinos Tinto',3);
+INSERT INTO post(fechaCreacion,contenido,descripcion,titulo,idusuario) VALUES ('2020-06-28', 'Catalogo cigarrillos','Lucky Strike','Suave',3);
+INSERT INTO post(fechaCreacion,contenido,descripcion,titulo,idusuario) VALUES ('2020-06-28', 'Catologo automoviles','Audi coupe','Deportivo',2);
 
-
--- tabla comentario
+-- 9. Crear una nueva tabla, llamada comentarios, con los atributos id, fecha y hora de creación,contenido, que se relacione con la tabla posts. (1 punto)
 
 CREATE TABLE comentarios(
 id SERIAL, 
@@ -74,3 +90,27 @@ idpost INT,
 PRIMARY KEY (id), 
 FOREIGN KEY (idpost) REFERENCES post(id)
 );
+
+--10. Crear 2 comentarios para el post de "Pamela" y 4 para "Carlos" (0.4 puntos)
+
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-25 00:00:00','Pamela Comentario 1',1);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-25 00:00:00','Pamela Comentario 2',2);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-26 00:00:00','Carlos Comentario 1',6);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-26 00:00:00','Carlos Comentario 2',6);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-26 00:00:00','Carlos Comentario 3',6);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-26 00:00:00','Carlos Comentario 4',6);
+
+-- 11. Crear un nuevo post para "Margarita" (1 punto)
+
+    -- se inserta usuario margarita
+    INSERT INTO usuario(nombre) VALUES ('Margarita');
+    -- se inserta nuevo post
+    INSERT INTO post(fechaCreacion,contenido,descripcion,titulo,idusuario) VALUES ('2020-06-28', 'Catologo flores','Margaritas silvestres','Flores',4);
+
+-- 12. Ingresar 5 comentarios para el post de Margarita. (1 punto)
+
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-27 00:00:00','Margarita Comentario 1',7);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-27 00:00:00','Margarita Comentario 2',7);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-27 00:00:00','Margarita Comentario 3',7);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-27 00:00:00','Margarita Comentario 4',7);
+INSERT INTO comentarios(fecha,contenido,idpost) VALUES ('2020-06-27 00:00:00','Margarita Comentario 5',7);
